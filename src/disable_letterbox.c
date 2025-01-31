f3a4f78d3b21437157af76e0fc6ebb581a9ff9d9
LT-Schmiddy <schmidalex1996@gmail.com>
2025-01-31 16:28:44 -0500

    fix for first-person aiming

--- src/disable_letterbox.c
+++ src/disable_letterbox.c
@@ -6,6 +6,6 @@ RECOMP_FORCE_PATCH void View_ApplyLetterbox(View* view) {
     return;
 }
 
-// RECOMP_PATCH void ShrinkWindow_Draw(GraphicsContext* gfxCtx) {
-//     return;
-// }
+RECOMP_FORCE_PATCH void ShrinkWindow_Draw(GraphicsContext* gfxCtx) {
+    return;
+}
